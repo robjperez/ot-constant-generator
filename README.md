6a3e3abbf70d6eb085ad4f744f4d0d6ace7da3fc
Roberto Perez Cubero <robj.perez@gmail.com>
2016-06-09 13:15:16 +0200

    First version

--- README.md
+++ README.md
@@ -2,11 +2,9 @@ Since in OSX 10.11 openssl is no longed included
 You will need to install it via brew and add these env vars
 
 *bash*
-export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
-export DEP_OPENSSL_INCLUDE=/usr/local/opt/openssl/include
-export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
+export OPENSSL_LIB_DIR=$(brew --prefix openssl)/lib
+export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)/include
 
 *fish*
-set -x OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
-set -x DEP_OPENSSL_INCLUDE /usr/local/opt/openssl/include
-set -x OPENSSL_ROOT_DIR /usr/local/opt/openssl
+set -x OPENSSL_LIB_DIR (brew --prefix openssl)/lib
+set -x OPENSSL_INCLUDE_DIR (brew --prefix openssl)/include
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,119 @@
 extern crate getopts;
+extern crate uuid;
+extern crate hyper;
+extern crate rustc_serialize;
+
 use getopts::Options;
 use std::env;
+use std::str::FromStr;
+use uuid::Uuid;
 
-trait Serializer {
-    fn serialize<T: Serializer>(&self, session_data: &SessionData<T>) -> String;
+use hyper::client::Client;
+use hyper::Url;
+use std::io::Read;
+
+use rustc_serialize::json::Json;
+
+enum Language {
+    Swift,
+    ObjC,
+    Java,
+    Kotlin
 }
 
-struct SessionData<T: Serializer> {
+impl FromStr for Language {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Language, ()> {
+        match s {
+            "swift" => Ok(Language::Swift),
+            "objc" => Ok(Language::ObjC),
+            "java" => Ok(Language::Java),
+            "kotlin" => Ok(Language::Kotlin),
+            _ => Err(())
+        }
+    }
+}
+
+enum Environment {
+    Meet,
+    OpentokRtc
+}
+
+impl FromStr for Environment {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Environment, ()> {
+        match s {
+            "meet" => Ok(Environment::Meet),
+            "opentokrtc" => Ok(Environment::OpentokRtc),
+            _ => Err(())
+        }
+    }
+}
+
+struct SessionData {
     api_key: String,
     token: String,
     session_id: String,
-    serializer: T
+    room: String
 }
 
-struct SwiftSerializer {}
-struct ObjcSerializer {}
-
-impl Serializer for SwiftSerializer {
-    fn serialize<T: Serializer>(&self, session_data: &SessionData<T>) -> String {
-        format!("let APIKEY={}\nlet TOKEN={}\nlet SESSIONID={}\n", 
-        session_data.api_key, 
-        session_data.token, 
-        session_data.session_id)
+impl SessionData {
+    fn get_out_string(&self, language: &Language) -> String {
+        match *language {
+            Language::ObjC =>
+            format!("// room: {}\nstatic NSString* const kApiKey = @\"{}\";\nstatic NSString* const kToken = @\"{}\";\nstatic NSString* const kSessionId = @\"{}\";\n",
+            self.room,
+            self.api_key,
+            self.token,
+            self.session_id),
+            Language::Swift =>
+            format!("// room: {}\nlet APIKEY = \"{}\"\nlet TOKEN = \"{}\"\nlet SESSIONID = \"{}\"\n",
+            self.room,
+            self.api_key,
+            self.token,
+            self.session_id),
+            Language::Java =>
+            format!("//room: {}\npublic static final String APIKEY = \"{}\";\npublic static final String TOKEN = \"{}\";\npublic static final String SESSION_ID = \"{}\";\n",
+            self.room,
+            self.api_key,
+            self.token,
+            self.session_id),
+            Language::Kotlin => format!("//room: {}\nval APIKEY = \"{}\";\nval TOKEN = \"{}\";\nval SESSION_ID = \"{}\";\n",
+            self.room,
+            self.api_key,
+            self.token,
+            self.session_id)
+        }
     }
-}
-
-impl Serializer for ObjcSerializer {
-    fn serialize<T: Serializer>(&self, session_data: &SessionData<T>) -> String {
-        format!("static const NSString *APIKEY=@\"{}\"\nstatic const NSString *TOKEN=@\"{}\"\nstatic const NSString *SESSIONID=@\"{}\"\n", 
-        session_data.api_key, 
-        session_data.token, 
-        session_data.session_id)
+    fn serialize(&self, lang: &Language) -> String {
+        self.get_out_string(lang)
     }
-}
 
-impl<T: Serializer> SessionData<T> {
-    fn serialize(&self) -> String {
-        self.serializer.serialize(&self)
-    }
-    
-    fn new(s: T) -> SessionData<T> {
+    fn new(env: &Environment, room: &String) -> SessionData {
+
+        let url = match *env {
+            Environment::Meet => format!("https://meet.tokbox.com/{}", room),
+            Environment::OpentokRtc => format!("https://opentokrtc.com/{}.json", room)
+        };
+        //println!(">>> {}", url);
+
+        let client = Client::new();
+        let mut res = client.get(Url::parse(url.as_ref()).unwrap()).send().unwrap();
+        let mut s = String::new();
+        res.read_to_string(&mut s).unwrap();
+
+        //println!("Response: {:?}", s);
+        let data = Json::from_str(s.as_ref()).unwrap();
+        let obj = data.as_object().unwrap();
+        let token = obj.get("token").unwrap().as_string().unwrap();
+        let sid = obj.get("sessionId").unwrap().as_string().unwrap();
+        let apikey = obj.get("apiKey").unwrap().as_string().unwrap();
+
         SessionData {
-            api_key: String::from(""),
-            token: String::from(""),
-            session_id: String::from(""),
-            serializer: s
+            api_key: String::from(apikey),
+            token: String::from(sid),
+            session_id: String::from(token),
+            room: String::from(room.as_ref())
         }
     }
 }
@@ -59,22 +128,25 @@ fn main() {
     let program = args[0].clone();
     let mut opts = Options::new();
     opts.reqopt("l", "language", "output language" ,"swift, objc, java, kotlin");
-    opts.optopt("e", "environment", "target env", "prod, dev");
+    opts.optopt("e", "environment", "target env", "meet, opentokrtc");
+    opts.optopt("r", "room", "room name", "STRING");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { print!("Error: {}", f.to_string()); print_usage(&program, &opts); panic!() }
     };
 
-    let mut session_data  = match matches.opt_str("l").unwrap().as_ref() {
-        "swift" => SessionData::new(SwiftSerializer{}),
-        "objc" => SessionData::new(ObjcSerializer{}),
-        _ => SessionData::new(SwiftSerializer{})
+    let env = match matches.opt_str("e") {
+        Some(e) => e.parse::<Environment>().unwrap(),
+        _ => Environment::OpentokRtc
     };
-    
-    session_data.api_key = String::from("A");
-    session_data.session_id = String::from("S");
-    session_data.token = String::from("T");
-    
-    print!("{}", session_data.serialize());
+
+    let room = match matches.opt_str("r") {
+        Some(r) => r,
+        _ => Uuid::new_v4().hyphenated().to_string()
+    };
+
+    let session_data = SessionData::new(&env, &room);
+    let lang = matches.opt_str("l").unwrap().parse::<Language>().unwrap();
+    print!("{}", session_data.serialize(&lang));
 }
